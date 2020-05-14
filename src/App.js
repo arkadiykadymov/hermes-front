16461ef84d82b9cd011a56d3f429707929067aeb
Daddy <arkadiymaestro@gmail.com>
2020-05-15 02:30:37 +0700

    Mod 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect, Link, withRouter } from 'react-router-dom';
 import Register from './components/Register';
+import Login from './components/Login';
 import './App.css';
 
 class App extends Component {
   render() {
+    isAuthenticated.authenticate();
+    console.log(isAuthenticated.isAuth);
+    console.log(localStorage.getItem("access_token"));
     return (
       <div className="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -25,21 +29,29 @@ class App extends Component {
                 <a class="nav-link" href="#">Link</a>
               </li>
             </ul>
-            <div class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Account
+            {isAuthenticated.isAuth ? (
+              <div>
+                Hello, {localStorage.getItem("username")}
+                <button onClick={isAuthenticated.sighnout}>log out</button>
+              </div>) :
+              (<div>
+                <div class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Account
               </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <div class="d-menu">
-                  <h6>Welcome to HiBuy online shop</h6>
-                  <button style={{ 'margin-left': '20px', 'margin-right': '20px' }} type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Register</button>
-                  <button type="button" class="btn btn-success">Login</button>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <div class="d-menu">
+                      <h6>Welcome to HiBuy online shop</h6>
+                      <button style={{ 'margin-left': '20px', 'margin-right': '20px' }} type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal-r">Register</button>
+                      <button style={{ 'margin-left': '20px', 'margin-right': '20px' }} type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal-l">Login</button>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
+              )}
           </div>
         </nav>
-        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="exampleModal-r" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
@@ -57,8 +69,39 @@ class App extends Component {
             </div>
           </div>
         </div>
+        <div class="modal fade" id="exampleModal-l" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Register form</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <Login />
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
+  }
+}
+
+const isAuthenticated = {
+  isAuth: false,
+  authenticate() {
+    if (localStorage.getItem("access_token"))
+      this.isAuth = true;
+
+  },
+  sighnout() {
+    localStorage.clear();
+    window.location.reload();
   }
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -4,25 +4,15 @@ import axios from 'axios';
 export default class Registration extends React.Component {
     constructor(props) {
         super(props);
-        this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
-        this.handleFullNameChange = this.handleFullNameChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
         this.handleUserNameChange = this.handleUserNameChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
-            email: '',
             password: '',
-            fullName: '',
             userName: ''
         };
     }
 
-    handleFullNameChange(e) {
-        this.setState({ fullName: e.target.value })
-    }
-    handleEmailChange(e) {
-        this.setState({ email: e.target.value })
-    }
     handlePasswordChange(e) {
         this.setState({ password: e.target.value })
     }
@@ -32,17 +22,18 @@ export default class Registration extends React.Component {
 
     handleSubmit(event) {
         event.preventDefault();
-        axios.post('http://127.0.0.1:8000/api/users/registration',
+        axios.post('http://127.0.0.1:8000/api/auth/signin',
             {
+                username: this.state.userName,
                 password: this.state.password,
-                email: this.state.email,
-                fullName: this.state.fullName,
-                username: this.state.userName
             }, {
             'Content-Type': 'application/json',
         }
         ).then((response) => {
             console.log(response);
+            localStorage.setItem("username", response.data.response.username);
+            localStorage.setItem("access_token", response.data.response.token);
+            window.location.reload();
         });
     }
 
@@ -55,17 +46,8 @@ export default class Registration extends React.Component {
                         <input type="text" class="form-control" id="exampleInputUsername" placeholder="Username" onChange={this.handleUserNameChange} />
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={this.handleEmailChange} />
-                        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div class="form-group">
                         <label for="exampleInputPassword1">Password</label>
                         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" onChange={this.handlePasswordChange} />
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputName">Full Name</label>
-                        <input type="text" class="form-control" id="exampleInputName" placeholder="Full Name" onChange={this.handleFullNameChange} />
                     </div>
                     <button type="submit" class="btn btn-primary" onClick={this.handleSubmit}>Submit</button>
                 </form>
